082fb081a2f0c06467bcb04e48a66f01dc2509a5
Shaik Afroz Ahmed <saa.afrozshaik112@gmail.com>
2025-12-17 17:46:24 +0530

    feat: implement resume parsing and upload functionality

--- backend/src/main/java/com/lck/resume_autofill/controller/ResumeController.java
+++ backend/src/main/java/com/lck/resume_autofill/controller/ResumeController.java
@@ -4,14 +4,11 @@ import com.lck.resume_autofill.dto.EmployeeFormDTO;
 import com.lck.resume_autofill.service.ResumeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/resume")
 @RequiredArgsConstructor
